53470a6ce33484738cdbd18d55ab56e4748a209c
Andy O'Neill <aoneill01@gmail.com>
2021-12-04 20:17:08 -0500

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,69 @@
+<script>
+  import Button from "./Button.svelte";
+  import Card from "./Card.svelte";
+  import { parseInput } from "./bingo.js";
+
+  let complete = false;
+  let { selectedNumbers, boards } = parseInput();
+
+  async function playGames() {
+    for (const number of selectedNumbers) {
+      await new Promise(r => setTimeout(r, 300));
+      boards
+        .filter(board => !board.hasWon())
+        .forEach(board => board.play(number));
+      boards = boards;
+      const winners = boards.filter(board => board.hasWon());
+      if (boards.every(board => board.hasWon())) {
+        complete = true;
+        return;
+      }
+    }
+  }
+
+  async function refresh() {
+    complete = false;
+    boards = parseInput().boards;
+    await new Promise(r => setTimeout(r, 1500));
+    playGames();
+  }
+
+  playGames();
+</script>
+
+<style>
+  :global(body) {
+    background-color: #ddbea9;
+  }
+
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+
+  .grid {
+    display: inline-grid;
+    grid-template-columns: repeat(10, auto);
+    gap: 8px;
+  }
+
+  a,
+  a:visited {
+    color: #cb997e;
+  }
+</style>
+
+<main>
+  <div class="grid">
+    {#each boards as board}
+      <Card {board} />	
+    {/each}
+  </div>
+  
+  <div>
+    <a href="https://github.com/aoneill01/bingo">repository</a><br />
+    {#if complete}
+      <a href={'#'} on:click={refresh}>refresh</a>
+    {/if}
+  </div>
+</main>
---
+++ Card.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let board;
+
+  $: complete = board.hasWon();
+</script>
+
+<style>
+  .card {
+    background-color: #6b705c;
+    width: 75px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    padding: 5px;
+    gap: 2px;
+    transition: opacity 0.6s ease-in-out 1s;
+    font-size: 8px;
+    font-weight: bold;
+    color: #a5a58d;
+  }
+  .cell {
+    background-color: #ffe8d6;
+    aspect-ratio: 1;
+    transition: background-color 0.4s ease-in-out;
+  }
+
+  .cell.win {
+    background-color: #cb997e;
+  }
+
+  .selected {
+    background-color: #b7b7a4;
+  }
+
+  .complete {
+    opacity: 0.25;
+  }
+</style>
+
+<div class="card" class:complete={complete}>
+  <div>B</div>
+  <div>I</div>
+  <div>N</div>
+  <div>G</div>
+  <div>O</div>
+  {#each board.selectedSquares as row, r}
+    {#each row as selected, c}
+      <div class="cell" class:win={board.isWinningSquare(r, c)} class:selected={selected}>&nbsp;</div>
+    {/each}
+  {/each}
+</div>
